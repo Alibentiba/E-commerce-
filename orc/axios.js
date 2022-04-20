661b20f9d3c6b5538e36985538e8b2fa7edfd2f8
Alibentiba <99966047+Alibentiba@users.noreply.github.com>
2022-04-20 15:20:19 +0100

    delet

--- orc/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const instance = axios.create({
-  baseURL: "https://us-central1-clone-b7977.cloudfunctions.net/api",
-  //"http://localhost:5001/clone-b7977/us-central1/api",
-});
-export default instance;
