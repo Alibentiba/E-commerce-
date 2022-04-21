81f1dd5ee94865fbcc14714cb8ce4149b0d1a041
Alibentiba <99966047+Alibentiba@users.noreply.github.com>
2022-04-22 00:11:38 +0100

    deployemnt

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,11 +9,12 @@ const firebaseConfig = {
   projectId: "ecom-5267d",
   storageBucket: "ecom-5267d.appspot.com",
   messagingSenderId: "564032441617",
-  appId: "1:564032441617:web:5f51a8bc4f9c2a4e82d0b0",
-  measurementId: "G-TRZKM47F1S"
+  appId: "1:564032441617:web:ea1d3afac2ad72dc82d0b0",
+  measurementId: "G-C78V4S4BK6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth=getAuth(app)
 // const analytics = getAnalytics(app);
+// Import the functions you need from the SDKs you need
